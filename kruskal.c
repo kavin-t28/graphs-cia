d2c39d7eae8cdb9a8c72ae10ef01e6fdb4a48b1e
Kavin <69395162+kavin-t28@users.noreply.github.com>
2023-01-18 11:24:20 +0530

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,56 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_VERTICES 100
-#define MAX_EDGES 5000
-
-typedef struct {
-  int u, v, w;
-} Edge;
-
-Edge edges[MAX_EDGES];
-int n, m;
-
-int find(int parent[], int i) {
-    if (parent[i] == -1) return i;
-    return find(parent, parent[i]);
+#include<stdio.h>
+#include<limits.h>
+void swap(int*x,int*y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
 }
 
-void Union(int parent[], int x, int y) {
-    int xset = find(parent, x);
-    int yset = find(parent, y);
-    if(xset != yset)
-        parent[xset] = yset;
-}
-
-int kruskal() {
-    int i, e = 0, u, v;
-    int ans = 0;
-    for (i = 0; i < m; i++) {
-        u = find(parent, edges[i].u);
-        v = find(parent, edges[i].v);
-        if (u != v) {
-            ans += edges[i].w;
-            Union(parent, u, v);
-            e++;
+void bubbleSort(int arr[],int src[],int dst[], int n){
+    int i, j;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(&arr[j], &arr[j + 1]);
+                swap(&src[j], &src[j + 1]);
+                swap(&dst[j], &dst[j + 1]);
+            }
         }
-        if (e == n - 1) break;
-    }
-    return ans;
 }
-
-int main() {
-    int i;
-    scanf("%d %d", &n, &m);
-    for (i = 0; i < m; i++) {
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
-    }
-    qsort(edges, m, sizeof(Edge), comp);
-    int s;
-    scanf("%d", &s);
-    int ans = kruskal();
-    printf("The minimum spanning tree weight is : %d \n", ans);
-    return 0;
 }
+int findabsroot(int v,int parent[]){
+    if(parent[v]==-1)
+		return v;
+	return findabsroot(parent[v],parent);
+}
+void union1(int from,int to,int parent[]){
+	from = findabsroot(from,parent);
+	to= findabsroot(to,parent);
+	parent[from]=to;
+}
+ int main(){
+  
+ 
+  int n;
+  printf("enter no of vertices: ");
+  scanf("%d",&n);
 
+  int edges;
+  int smst[n],dmst[n],parent[n];
+  printf("enter no of edges: ");
+  scanf("%d",&edges);
+
+  for(int i=0;i<edges;i++){
+       parent[i]=-1;
+  }
+ int src[7]={0,0,1,1,1,2,2,2,4};
+ int dst[7]={1,2,2,3,4,1,3,4,3};
+ int cost[7]={4,2,3,2,3,1,4,5,-5};
+ 
+  bubbleSort(cost,src,dst,edges);
+  for(int i=0;i<n-1;i++){
+      int a=findabsroot(src[i],parent);
+      int b=findabsroot(dst[i],parent);
+      if(a!=b){
+          smst[i]=src[i];
+          dmst[i]=dst[i];
+          union1(src[i],dst[i],parent);
+      }
+  }
+  for(int i=0;i<n-1;i++){
+    printf("%d --> %d ",smst[i],dmst[i]);
+    printf("\n");
+}
+  return 0;
+ }
